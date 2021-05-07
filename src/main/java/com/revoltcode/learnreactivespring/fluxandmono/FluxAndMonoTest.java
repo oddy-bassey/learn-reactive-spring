29ca466a3c37943f1f52421c5643a78eea8ff5f2
@revoltcode <oddybassey@gmail.com>
2021-05-07 05:37:15 +0100

    passing flux after throwing an error. closes #3

--- src/main/java/com/revoltcode/learnreactivespring/fluxandmono/FluxAndMonoTest.java
+++ src/main/java/com/revoltcode/learnreactivespring/fluxandmono/FluxAndMonoTest.java
@@ -9,6 +9,7 @@ public class FluxAndMonoTest {
     public void fluxTest(){
         Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring")
                 .concatWith(Flux.error(new RuntimeException("Exception Occurred!")))
+                .concatWith(Flux.just("After error"))
                 .log();
 
         stringFlux.subscribe(System.out::println, (e) -> System.out.println(e));
